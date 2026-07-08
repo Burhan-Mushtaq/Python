37af61eb8e4621ca6a8be0d66aec1934a311e70e
Burhan Mushtaq <techbmr6@gmail.com>
2026-07-08 23:03:39 +0530

    Chapter 4 - Problem 2

---
+++ 21_Unique_numbers.py
@@ -0,0 +1,14 @@
+s = set()
+
+num = input("Enter number 1 : ")
+s.add(int(num))
+num = input("Enter number 2 : ")
+s.add(int(num))
+num = input("Enter number 3 : ")
+s.add(int(num))
+num = input("Enter number 4 : ")
+s.add(int(num))
+num = input("Enter number 5 : ")
+s.add(int(num))
+
+print(s)
