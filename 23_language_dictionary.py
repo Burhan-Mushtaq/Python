014205b59cfbc0694dd1b6388f69bb6f21be4706
Burhan Mushtaq <techbmr6@gmail.com>
2026-07-08 23:10:46 +0530

    Chapter 4 - Problem 4

---
+++ 23_language_dictionary.py
@@ -0,0 +1,15 @@
+d = {}
+
+name = input("Enter 1st Friends name : ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter 2nd Friends name : ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+name = input("Enter 3rd Friends name : ")
+lang = input("Enter Language name: ")
+d.update({name : lang})
+
+print(d)
