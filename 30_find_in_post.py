b8628f368ae1520504bbbca95ca16b7e32c990a5
Burhan Mushtaq <techbmr6@gmail.com>
2026-07-09 12:59:20 +0530

    Chapter 5 - Problem 7

---
+++ 30_find_in_post.py
@@ -0,0 +1,6 @@
+post  = "Hey harry bhai is very good teacher of python"
+
+if("Harry".lower() in post.lower()):
+    print("This post is talking about Harry bhai")
+else:
+    print("This post is not talking about Harry bhai")
