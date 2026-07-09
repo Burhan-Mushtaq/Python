af5479ed020e3ae7f0c3226cb2c684da4fc4dae1
Burhan Mushtaq <techbmr6@gmail.com>
2026-07-09 12:46:30 +0530

    Chapter 5 - Problem 5

---
+++ 28_name_in_list.py
@@ -0,0 +1,8 @@
+l = ["Harry" , "Rohan" , "shubam", "Roshni"]
+
+name = input("Enter your name: ")
+
+if(name.upper in l):
+    print("Your name is in list")
+else:
+    print("Your name is not in the list")
